10cca6b26ddda84219f4b3cb214bf54d9e487839
Victoria Kharchenko <NikaElt@yandex.ua>
2015-11-29 20:40:39 +0200

    New GUI

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -16,8 +17,10 @@ public class MainForm extends JFrame implements Observer{
     private static javax.swing.JButton dissconectButton = new javax.swing.JButton();
     private javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
     private javax.swing.JPanel  jPanel2 = new javax.swing.JPanel();
-    private javax.swing.JScrollPane jScrollPane3 = new javax.swing.JScrollPane();
+    private javax.swing.JList jList1 = new javax.swing.JList();
+    private javax.swing.JScrollPane jScrollPane1 = new javax.swing.JScrollPane();
     private javax.swing.JScrollPane jScrollPane2 = new javax.swing.JScrollPane();
+    private javax.swing.JScrollPane jScrollPane3 = new javax.swing.JScrollPane();
     private javax.swing.JButton logInButton = new javax.swing.JButton();
     private javax.swing.JTextField  logInText = new javax.swing.JTextField();
     private static javax.swing.JTextArea   messageText = new javax.swing.JTextArea();
@@ -34,16 +37,21 @@ public class MainForm extends JFrame implements Observer{
     // End of variables declaration
 
     public MainForm() throws IOException {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
         setBackground(new java.awt.Color(102, 102, 102));
         setFocusable(false);
+        java.awt.GridBagLayout layout = new java.awt.GridBagLayout();
+        layout.columnWidths = new int[] {0, 0, 0};
+        layout.rowHeights = new int[] {0, 1, 0};
+        getContentPane().setLayout(layout);
 
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Settings", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Monotype Corsiva", 0, 18))); // NOI18N
+        jPanel1.setBackground(new java.awt.Color(51, 51, 51));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Settings", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Monotype Corsiva", 1, 18), new java.awt.Color(255, 255, 255))); // NOI18N
 
-
+        logInButton.setBackground(new java.awt.Color(153, 153, 153));
         logInButton.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
         logInButton.setText("Log in");
         logInButton.addActionListener(new java.awt.event.ActionListener() {
@@ -52,6 +60,7 @@ public class MainForm extends JFrame implements Observer{
             }
         });
 
+        connectButton.setBackground(new java.awt.Color(153, 153, 153));
         connectButton.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
         connectButton.setText("Connect");
         connectButton.addActionListener(new java.awt.event.ActionListener() {
@@ -60,6 +69,7 @@ public class MainForm extends JFrame implements Observer{
             }
         });
 
+        dissconectButton.setBackground(new java.awt.Color(153, 153, 153));
         dissconectButton.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
         dissconectButton.setText("Dissconect");
         dissconectButton.setEnabled(false);
@@ -69,14 +79,30 @@ public class MainForm extends JFrame implements Observer{
             }
         });
 
+        logInText.setBackground(new java.awt.Color(51, 51, 51));
+        logInText.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
+        logInText.setForeground(new java.awt.Color(204, 204, 204));
+
+        IPText.setBackground(new java.awt.Color(51, 51, 51));
+        IPText.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
+        IPText.setForeground(new java.awt.Color(204, 204, 204));
         IPText.setText("Enter IP");
+
+        jList1.setBackground(new java.awt.Color(54, 49, 49));
+        jList1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Friends", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Monotype Corsiva", 0, 16), new java.awt.Color(255, 255, 255))); // NOI18N
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addGap(0, 10, Short.MAX_VALUE)
+                                .addGap(0, 11, Short.MAX_VALUE)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                         .addComponent(logInButton, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGroup(jPanel1Layout.createSequentialGroup()
@@ -87,7 +113,8 @@ public class MainForm extends JFrame implements Observer{
                                 .addContainerGap()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addComponent(logInText)
-                                        .addComponent(IPText))
+                                        .addComponent(IPText)
+                                        .addComponent(jScrollPane1))
                                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -103,10 +130,23 @@ public class MainForm extends JFrame implements Observer{
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(connectButton)
                                         .addComponent(dissconectButton))
-                                .addContainerGap(148, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        sendButton.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jPanel1, gridBagConstraints);
+
+        jPanel2.setBackground(new java.awt.Color(51, 51, 51));
+
+
+        sendButton.setBackground(new java.awt.Color(153, 153, 153));
+        sendButton.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
         sendButton.setText("Send");
         sendButton.setEnabled(false);
         sendButton.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -124,9 +164,29 @@ public class MainForm extends JFrame implements Observer{
             }
         });
 
+        clearButton.setBackground(new java.awt.Color(153, 153, 153));
+        clearButton.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        clearButton.setText("Clear");
+        clearButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearButtonActionPerformed(evt);
+            }
+        });
+
+        messageText.setBackground(new java.awt.Color(51, 51, 51));
         messageText.setColumns(20);
+        messageText.setFont(new java.awt.Font("Comic Sans MS", 0, 12)); // NOI18N
+        messageText.setForeground(new java.awt.Color(204, 204, 204));
         messageText.setRows(5);
-        messageText.setEnabled(false);
+        messageText.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                messageTextKeyPressed(evt);
+            }
+
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                messageTextKeyTyped(evt);
+            }
+        });
         jScrollPane2.setViewportView(messageText);
 
         chatBox.setColumns(20);
@@ -141,45 +201,39 @@ public class MainForm extends JFrame implements Observer{
                         .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jScrollPane3)
-                                        .addGroup(jPanel2Layout.createSequentialGroup()
-                                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 356, Short.MAX_VALUE)
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                                .addGap(0, 0, Short.MAX_VALUE)
+                                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                                         .addComponent(sendButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                        .addComponent(clearButton, javax.swing.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE))
-                                                .addGap(0, 2, Short.MAX_VALUE)))
+                                                        .addComponent(clearButton, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))))
                                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
                 jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jScrollPane3)
-                                .addGap(18, 18, 18)
-                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                         .addGroup(jPanel2Layout.createSequentialGroup()
-                                                .addComponent(sendButton)
-                                                .addGap(11, 11, 11)
-                                                .addComponent(clearButton))
+                                                .addComponent(sendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(clearButton, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(6, 6, 6))
                                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap())
+                                .addContainerGap(14, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jPanel2, gridBagConstraints);
 
         pack();
     }// </editor-fold>
@@ -194,7 +248,7 @@ public class MainForm extends JFrame implements Observer{
     }
 
     private void clearButtonActionPerformed(java.awt.event.ActionEvent evt) {
-        messageText.setText( connect.getSc().getInetAddress().toString());
+        messageText.setText( connect.getSc().getInetAddress().toString()); //WTF??????
     }
 
     private void logInButtonActionPerformed(java.awt.event.ActionEvent evt) {
@@ -240,7 +294,17 @@ public class MainForm extends JFrame implements Observer{
         sendButton.setEnabled(true);
     }
 
+    private void messageTextKeyTyped(java.awt.event.KeyEvent evt) {
+        // TODO add your handling code here:
+    }
 
+    private void messageTextKeyPressed(java.awt.event.KeyEvent evt) {
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER){
+            connect.sendMsg(messageText.getText() + "\n");
+            chatBox.append(messageText.getText() + "\n");
+            messageText.setText("");
+        }
+    }
 
     /**
      * @param args the command line arguments
